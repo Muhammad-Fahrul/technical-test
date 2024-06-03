d8d3caf85fd06720cabc428e996347dad22ccde2
Muhammad-Fahrul <fahruldev22@gmail.com>
2024-06-03 17:51:10 +0800

    minor update, fixing id of each item

--- backend/models/notes.js
+++ backend/models/notes.js
@@ -1,21 +1,21 @@
 export default [
   {
-    id: 2,
+    id: 1,
     title: 'note2',
     desc: 'note2',
   },
   {
-    id: 3,
+    id: 2,
     title: 'note3',
     desc: 'note3',
   },
   {
-    id: 4,
+    id: 3,
     title: 'note4',
     desc: 'note4',
   },
   {
-    id: 5,
+    id: 4,
     title: 'note5',
     desc: 'note5',
   },
